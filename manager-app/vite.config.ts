1479c770514ff6e0b49318724131de7a647b89b2
邵😉😉 <736083950@qq.com>
2023-02-19 21:22:25 +0800

    后台管理

--- manager-app/vite.config.ts
+++ manager-app/vite.config.ts
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: { // 本地开发环境通过代理实现跨域
-      // 正则表达式写法
-      '/api': {
-        target: 'https://admin.ep-back.sandbox.100mzhan.com.cn/', // 后端服务实际地址
-        changeOrigin: true, //开启代理
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
 })
